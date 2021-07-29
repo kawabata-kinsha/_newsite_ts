84d47a83ea504da39179db36fc9906686ab709b4
Ryo Kawabata <kawabata@kinsha.co.jp>
2021-07-29 18:57:58 +0900

    PHPテンプレート修正

--- www/php/modules/js.php
+++ www/php/modules/js.php
@@ -1,3 +1,3 @@
-<script src="/assets/js_lib/jquery-3.5.1.min.js" type="text/javascript"></script>
-<script src="/assets/js_lib/slick.min.js" type="text/javascript"></script>
-<script src="/assets/js/app.js?ver=<?php echo $jsVer;?>"></script>
+<script src="<?php echo $root_folder; ?>/assets/js_lib/jquery-3.5.1.min.js" type="text/javascript"></script>
+<script src="<?php echo $root_folder; ?>/assets/js_lib/slick.min.js" type="text/javascript"></script>
+<script src="<?php echo $root_folder; ?>/assets/js/app.js?ver=<?php echo $jsVer;?>"></script>
