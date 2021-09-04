8fbe2a77cc670fb50a5cd38ea550bf9aedb27a41
Ryo Kawabata <kawabata@kinsha.co.jp>
2021-09-04 09:55:15 +0900

    ビルド後のjsファイル名を変更

--- www/php/modules/js.php
+++ www/php/modules/js.php
@@ -1,3 +1,3 @@
 <script src="<?php echo $root_folder; ?>/assets/js_lib/jquery-3.5.1.min.js" type="text/javascript"></script>
 <script src="<?php echo $root_folder; ?>/assets/js_lib/slick.min.js" type="text/javascript"></script>
-<script src="<?php echo $root_folder; ?>/assets/js/app.js?ver=<?php echo $jsVer;?>"></script>
+<script src="<?php echo $root_folder; ?>/assets/js/main.js?ver=<?php echo $jsVer;?>"></script>
