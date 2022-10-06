4fb4d92e6c89c9fec0e3bb9931323f303e10d6f0
Ryo Kawabata <kawabata@kinsha.co.jp>
2022-10-06 15:22:25 +0900

    PHP変数名を修正

--- www/php/modules/_header.php
+++ www/php/modules/_header.php
@@ -12,7 +12,7 @@
 <!-- ogp -->
 <meta content="<?php echo $siteTitle; ?>" property="og:title" />
 <meta content="website" property="og:type" />
-<meta content="<?php echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"]; ?><?php echo $img_folder; ?>/common/ogp.png" property="og:image" />
+<meta content="<?php echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"]; ?><?php echo $rootImg; ?>/common/ogp.png" property="og:image" />
 <meta content="<?php echo $siteDescription; ?>" property="og:description" />
 <meta content="" property="og:url" />
 <meta content="<?php echo $siteTitle; ?>" property="og:site_name" />
@@ -20,23 +20,23 @@
 <!-- twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta content="<?php echo $twitterId; ?>" name="twitter:site" />
-<meta property="twitter:url" content="<?php echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"]; ?><?php echo $root_folder; ?>/" />
+<meta property="twitter:url" content="<?php echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"]; ?><?php echo $root; ?>/" />
 <meta property="twitter:title" content="<?php echo $siteTitle; ?>" />
 <meta property="twitter:description" content="<?php echo $siteDescription; ?>" />
-<meta property="twitter:image" content="<?php echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"]; ?><?php echo $img_folder; ?>/common/ogp.png" />
+<meta property="twitter:image" content="<?php echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"]; ?><?php echo $rootImg; ?>/common/ogp.png" />
 
 <!-- icon -->
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $img_folder; ?>/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $img_folder; ?>/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $img_folder; ?>/favicons/favicon-16x16.png">
-<link rel="manifest" crossorigin="use-credentials" href="<?php echo $img_folder; ?>/favicons/site.webmanifest">
-<link rel="mask-icon" href="<?php echo $img_folder; ?>/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $rootImg; ?>/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $rootImg; ?>/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $rootImg; ?>/favicons/favicon-16x16.png">
+<link rel="manifest" crossorigin="use-credentials" href="<?php echo $rootImg; ?>/favicons/site.webmanifest">
+<link rel="mask-icon" href="<?php echo $rootImg; ?>/favicons/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
-<link href="<?php echo $img_folder; ?>/favicons/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<link href="<?php echo $rootImg; ?>/favicons/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 
 <!-- css -->
-<link rel="stylesheet" href="<?php echo $root_folder; ?>/assets/css/style.css?ver=<?php echo $cssVer;?>" />
+<link rel="stylesheet" href="<?php echo $root; ?>/assets/css/style.css?ver=<?php echo $cssVer;?>" />
 
 </head>
 
