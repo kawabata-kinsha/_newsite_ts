a6475e4a944b5c940234590ec613470a9a33ede8
Ryo Kawabata <kawabata@kinsha.co.jp>
2022-10-26 15:17:41 +0900

    リンク関連修正

--- www/php/modules/_footer.php
+++ www/php/modules/_footer.php
@@ -2,9 +2,9 @@
 <!-- /wrap -->
 
 
-<script src="<?php echo $root_folder; ?>/assets/js_lib/jquery-3.5.1.min.js" type="text/javascript"></script>
-<script src="<?php echo $root_folder; ?>/assets/js_lib/slick.min.js" type="text/javascript"></script>
-<script src="<?php echo $root_folder; ?>/assets/js/main.js?ver=<?php echo $jsVer;?>"></script>
+<script src="<?php echo $root; ?>/assets/js_lib/jquery-3.5.1.min.js" type="text/javascript"></script>
+<script src="<?php echo $root; ?>/assets/js_lib/slick.min.js" type="text/javascript"></script>
+<script src="<?php echo $root; ?>/assets/js/main.js?ver=<?php echo $jsVer;?>"></script>
 
 
 </body>
