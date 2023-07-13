b0e6d398f96edc44a27bb98a4606a14c1ec7c87f
Ryo Kawabata <kawabata@kinsha.co.jp>
2023-07-13 12:26:14 +0900

    pjax

--- src/ts/modules/_pjax.ts
+++ src/ts/modules/_pjax.ts
@@ -1,40 +1,35 @@
 export class Pjax {
-	$window: JQuery<Window>;
-	$document: JQuery<Document>;
-	$link: JQuery;
-	link: string;
-	target: string;
-	delaytime: number;
+	protected link: string;
+	protected target: string;
+	protected delaytime: number;
 
 
 	constructor(link: string, target: string, delaytime: number){
-		this.$window = $(window);
-		this.$document = $(document);
-		this.$link = $(link);
 		this.link = link;
 		this.target = target;
 		this.delaytime = delaytime;
 		this.init();
 	}
 
-	init(){
-		this.$window.on('load', () => {
+	private init(){
+		window.addEventListener("load", () => {
 			this.onLoad();
 		});
 
-		this.$document.on('click', this.link, (e)=>{
-			this.onClick(e, e.currentTarget);
+		document.querySelectorAll(this.link).forEach((link) => {
+			link.addEventListener('click', (e) => {
+				this.onClick(e, e.target);
+			});
 		});
 
-		this.$window.on('popstate', (e) => {
+		window.addEventListener('popstate',(e) => {
 			this.onPopState(e);
 		});
 	}
 
-	onClick(e: any, self: string){
+	private onClick(e: any, self: any){
 		e.preventDefault();
-
-		const path: string = $(self).attr('href');
+		const path: string = self.getAttribute('href');
 		const title: string = null;
 		if(path === ''){
 			console.log(false);
@@ -48,50 +43,66 @@ export class Pjax {
 		}
 	}
 
-	onPopState(e: any){
+	private onPopState(e: any){
 		this.beforeFetch();
 		setTimeout(() => {
 			this.ajax(this.getTarget(e));
 		}, this.delaytime)
 	}
 
-	ajax(path: string){
-		// $(this.target).load(path + ' .wrap');
-		const request = new XMLHttpRequest();
-		request.responseType = 'document';
-		request.open('GET', path, true);
-		request.onload = () => {
-			const nextTitle = request.responseXML.querySelector('title').innerText;
-			const bodyClassList = request.responseXML.querySelector('body').classList['value'];
-			const nextContents = request.responseXML.querySelector(this.target)
-			console.log(bodyClassList);
-			document.title = nextTitle;
-			document.querySelector(this.target).innerHTML = nextContents.innerHTML;
-			document.querySelector('body').setAttribute('class', bodyClassList);
-
+	private ajax(path: string){
+		const promise = fetch(path);
+		promise.then((response) => {
+			return response.text();
+		}).then((data) => {
+			const parser = new DOMParser();
+			const html = parser.parseFromString(data, "text/html");
+			this.changeTitle(html);
+			this.changeBodyClass(html);
+			this.changeContents(html);
 			this.afterFetch();
-        }
-		request.send();
+		});
+		// const request = new XMLHttpRequest();
+		// request.responseType = 'document';
+		// request.open('GET', path, true);
+		// request.onload = () => {
+		// 	const html = request.responseXML;
+		// 	this.changeTitle(html);
+		// 	this.changeBodyClass(html);
+		// 	this.changeContents(html);
+		// 	this.afterFetch();
+        // }
+		// request.send();
 	}
 
-	getCurrentPath(){
-		return location.pathname;
-	}
-
-	pushStateObj(path: string){
+	private pushStateObj(path: string){
 		const state: { 'path': string} = {'path': path};
 		return state;
 	}
 
-	getTarget(e: { originalEvent: { state: any; }; }){
-		var state = e.originalEvent.state;
+	private getTarget(e: any){
+		var state = e.state;
 		return state.path;
 	}
 
-	onLoad(){}
+	private changeTitle(html: Document){
+		const nextTitle = html.querySelector('title').innerText;
+		document.title = nextTitle;
+	}
+	private changeBodyClass(html: Document){
+		const bodyClassList = html.querySelector('body').classList['value'];
+		document.querySelector('body').setAttribute('class', bodyClassList);
+	}
 
-	beforeFetch(){}
+	private changeContents(html: Document){
+		const nextContents =  html.querySelector(this.target).innerHTML;
+		document.querySelector(this.target).innerHTML = nextContents;
+	}
 
-	afterFetch(){}
+	protected onLoad(){}
+
+	protected beforeFetch(){}
+
+	protected afterFetch(){}
 
 }
